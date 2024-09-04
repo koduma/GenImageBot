b31f990762833c90391c79bffa2ecb795974b481
tekitouk <47982907+koduma@users.noreply.github.com>
2024-09-04 12:24:34 +0900

    Add files via upload

---
+++ GenImage.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+img = Image.open('')
+
+pixels = img.load()
+
+w, h = img.size
+
+for i in range(w):#幅
+    for j in range(h):#高さ
+        pixels[i, j] = (0, 255, 0)#RGB
+        pixel_value = img.getpixel((i, j))
+        #print(str(pixel_value))
+
+img.save('')
